c8d471ce53c4eb75cd23484da5c0ed026a39a8d0
felipefonteslisboa <felipefonteslisboa@gmail.com>
2024-05-10 13:35:25 -0300

    Efeito header

--- script.js
+++ script.js
@@ -1,55 +1,50 @@
-const headerEfeitoScrol = document.getElementById("header")
-window.addEventListener("load", () => {
-    headerEfeitoScrol.style.top = "0%"
+const menuLateral = document.getElementById("menuLateral");
+const buttonAbrirMenuLateral = document.getElementById("abrirMenuLateral");
+const buttonFecharMenuLateral = document.getElementById(
+  "buttonFecharMenuLateral"
+);
+const carinhoDeCompras = document.getElementById("CarinhoCompras");
+const buttonAbrirCarinho = document.getElementById("abrirCarinho");
+const buttonFecharCarrinho = document.getElementById("ButtonFecharCarinho");
 
-})
+const headerEfeitoScrol = document.getElementById("header");
+window.addEventListener("load", () => {
+  headerEfeitoScrol.style.top = "0%";
+});
+
+const abrirCarinho = () => {
+  buttonAbrirCarinho.addEventListener("click", () => {
+    carinhoDeCompras.style.left = "81%";
+  });
+};
+const fecharCarrinho = () => {
+  buttonFecharCarrinho.addEventListener("click", () => {
+    carinhoDeCompras.style.left = "100%";
+  });
+};
+const abrirMenu = () => {
+  buttonAbrirMenuLateral.addEventListener("click", () => {
+    menuLateral.style.left = "0%";
+  });
+};
+
+const fecharMenu = () => {
+  menuLateral.style.left = "-20%";
+};
+buttonFecharMenuLateral.addEventListener("click", fecharMenu);
+
+abrirCarinho();
+fecharCarrinho();
+abrirMenu();
+fecharMenu();
 
 // const imgDoCarossel = document.getElementById("imgCarossel")
-
 // const contagem = window.addEventListener("load",()=>{
 //   setTimeout(() => {
 //     if (imgDoCarossel.src === "./assets/img/img1.jpg" ) {
 //      return setTimeout(() => {
-//       imgDoCarossel.src === "./assets/img/img2.jpg" 
+//       imgDoCarossel.src === "./assets/img/img2.jpg"
 //       }, 3000);
 //      }
 // }, 3000);
 // })
-
-const carinhoDeCompras = document.getElementById("CarinhoCompras");
-const buttonAbrirCarinho = document.getElementById("abrirCarinho");
-const fecharCarrinho = document.getElementById("ButonFecharCarinho");
-
-buttonAbrirCarinho.addEventListener("click", () => {
-  carinhoDeCompras.style.left = "81%";
-  abrirMenu()
-});
-
-fecharCarrinho.addEventListener("click", () => {
-  carinhoDeCompras.style.left = "100%";
-});
-
-
-const menuLateral = document.getElementById("menuLateral");
-const fecharMenuLateral = document.getElementById("fecharMenuLateral");
-
-fecharMenuLateral.addEventListener("click", () => {
-  menuLateral.style.left = "-20%";
-});
-
-
-
-const abrirMenu = () => {
-  const buttonAbrirMenuLateral = document.getElementById("abrirMenuLateral");
-
-buttonAbrirMenuLateral.addEventListener("click", () => {
-  menuLateral.style.left = "0%";
-});
-}
-
-
-
-
-
-
-
